27f09da55df1b6ef09e7c98bfe2c2e9e82026eee
hcsyash <suyashsrivastava25@gmail.com>
2021-07-26 18:36:06 +0530

    Adding Socket Logic for IP

--- aws_adfs/login.py
+++ aws_adfs/login.py
@@ -146,13 +146,11 @@ def login(
     )
 
     _verification_checks(config)
-    hostname = socket.gethostname()
-    print(hostname)
-    #local_ip = socket.gethostbyname('HIBACL131230')
+
     #click.echo('Trying re-authenticating using an existing ADFS session at IP {} and Host {}'.format(local_ip, hostname))
     # Try re-authenticating using an existing ADFS session
     principal_roles, assertion, aws_session_duration = authenticator.authenticate(config, assertfile=assertfile)
-
+    click.echo('Trying re-authenticating using an existing ADFS session')
     # If we fail to get an assertion, prompt for credentials and try again
     if assertion is None:
         password = None
@@ -165,7 +163,20 @@ def login(
             config.adfs_user, password = _file_user_credentials(config.profile, authfile)
 
         config.adfs_user = click.prompt(text='Username', type=str, default=config.adfs_user)
-        
+        if not config.adfs_user:
+            try: 
+                hostname = socket.gethostname()
+                print(hostname)
+                local_ip = socket.gethostbyname(hostname)
+                if re.match(r"(^192\.168\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])$)|(^172\.([1][6-9]|[2][0-9]|[3][0-1])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])$)|(^10\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])$)", local_ip):
+                    print('Extranet')
+                    config.adfs_user = click.prompt(text='HarmanEmail', type=str, default=config.adfs_user)
+                else:
+                    print('Intranet')
+                    config.adfs_user = click.prompt(text='HarmanId', type=str, default=config.adfs_user)
+            except:
+                print('Cannot retrieve the IP Address')
+                config.adfs_user = click.prompt(text='Usercode', type=str, default=config.adfs_user)
         #if not config.adfs_user:
         #    if re.match(r"(^192\.168\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])$)|(^172\.([1][6-9]|[2][0-9]|[3][0-1])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])$)|(^10\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])$)", local_ip):
         #        print('Extranet')
